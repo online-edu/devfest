00bb4adcae800506bf4a787e7f7ec5beaf3507e8
Hardik Pithva <hardikpthv@gmail.com>
2017-10-27 23:31:33 +0530

    chore: added form controls

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -6,10 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
-
+  designations: string[];
+  events: string[];
   constructor() { }
 
   ngOnInit() {
+    this.designations = [
+      'Student/Trainee engineer',
+      'Software Engineer (Web)',
+      'Software Engineer (Mobile)',
+      'Entrepreneur',
+      'Other Profession'
+    ];
+    this.events = [
+      'DevFest Ahmedabad 2016',
+      'DevFest Ahmedabad 2015',
+      'DevFest Ahmedabad 2014',
+      'DevFest Ahmedabad 2013',
+      'Women Techmakers: Ahmedabad',
+      'Google I/O Extended Ahmedabad',
+      'None'
+    ]
   }
 
 }
