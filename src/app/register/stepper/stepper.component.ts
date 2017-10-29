e6191efbb5b0e810fbae26424ff9d677cf0e4677
Hardik Pithva <hardikpthv@gmail.com>
2017-10-29 22:48:20 +0530

    feat: added stepper component

---
+++ src/app/register/stepper/stepper.component.ts
@@ -0,0 +1,65 @@
+import { Component, OnInit, forwardRef, Input, OnChanges } from '@angular/core';
+import {
+  ControlValueAccessor,
+  FormControl,
+  NG_VALUE_ACCESSOR,
+  NG_VALIDATORS
+} from '@angular/forms';
+
+@Component({
+  selector: 'stepper-control',
+  templateUrl: './stepper.component.html',  
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => StepperComponent),
+      multi: true
+    },
+    {
+      provide: NG_VALIDATORS,
+      useExisting: forwardRef(() => StepperComponent),
+      multi: true
+    }
+  ]
+})
+export class StepperComponent implements ControlValueAccessor {
+  @Input('value') _value = 0;
+
+  constructor() { }
+
+  propagateChange: any = () => { };
+  validateFn: any = () => { };
+
+  get value() {
+    return this._value;
+  }
+
+  set value(val) {
+    this._value = val;
+    this.propagateChange(val);
+  }
+
+  writeValue(value) {
+    if (value) {
+      this.value = value;
+    }
+  }
+
+  registerOnChange(fn) {
+    this.propagateChange = fn;
+  }
+
+  registerOnTouched() { }
+
+  increment() {
+    this.value++;
+  }
+
+  decrement() {
+    this.value != 0 ? this.value-- : 0;
+  }
+
+  validate(c: FormControl) {
+    return this.validateFn(c);
+  }
+}
